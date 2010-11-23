5e58d129fe669d0a13c74e70fec7a54a2bb96ebe
Sten Roger Sandvik <srs@enonic.com>
2010-11-23 16:35:36 +0100

    Converted plugin to new format.

--- src/main/java/com/enonic/cms/plugin/confluence/ConfluenceActivator.java
+++
@@ -1,8 +0,0 @@
-package com.enonic.cms.plugin.confluence;
-
-import com.enonic.cms.plugin.spring.SpringPluginActivator;
-
-public final class ConfluenceActivator 
-    extends SpringPluginActivator
-{
-}
